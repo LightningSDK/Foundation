ee216fd4cc29f98860aba052a38ff7e7336d6b65
DAB <dab@dab.la>
2020-06-30 23:46:57 -0400

    Moved media query init to init.js

--- View/Foundation.php
+++ View/Foundation.php
@@ -6,7 +6,6 @@ use lightningsdk\core\View\JS;
 
 class Foundation {
     public static function init() {
-        // TODO: delete window.default is a hack because for some reason it is set by foundation.
-        JS::startup("MediaQuery._reInit();lightning.foundation.init();", ['lightningsdk/foundation' => 'vendor/zurb/foundation/dist/js/foundation.min.js', 'document']);
+        JS::startup("lightning.foundation.init();", ['lightningsdk/foundation' => 'vendor/zurb/foundation/dist/js/foundation.min.js', 'document']);
     }
 }
